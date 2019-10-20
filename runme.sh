d6bd6d54fca3d0f178ef15b79292b812d96a268b
Gianpaolo Macario <gmacario@gmail.com>
2019-10-20 17:34:35 +0000

    Add (in comment) one TODO
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- runme.sh
+++ runme.sh
@@ -13,6 +13,8 @@ PATH="/c/Program Files (x86)/Microsoft Visual Studio/Shared/Python37_64:$PATH"
 # echo "DEBUG: After setting Python37_64 in PATH"
 # echo $PATH
 
+# TODO: Make sure Python 3.7.x or greater is available as `python` in `PATH`
+
 echo "DEBUG: Browse http://localhost:8000/"
 python -m http.server
 
